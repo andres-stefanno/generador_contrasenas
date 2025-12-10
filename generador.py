868e6744c05950d54480a33aac960efd610ccd17
Andres Inapanta <andresinapanta@MacBook-Air-de-Andres.local>
2025-12-09 22:26:11 -0500

    Corrección del código a Python real

--- generador.py
+++ generador.py
@@ -2,19 +2,19 @@ import random
 import string
 
 # ==============================
-# GENERADOR DE CONTRASEÑAS
+#  GENERADOR DE CONTRASEÑAS
 # ==============================
 
 print("=== GENERADOR DE CONTRASEÑAS ===")
 
-# -------- ENTRADA --------
+# ---------- ENTRADA ----------
 longitud = int(input("Ingrese la longitud de la contraseña: "))
 
 usar_letras = input("¿Desea usar letras? (s/n): ").lower()
 usar_numeros = input("¿Desea usar números? (s/n): ").lower()
 usar_simbolos = input("¿Desea usar símbolos? (s/n): ").lower()
 
-# -------- PROCESAMIENTO --------
+# ---------- PROCESAMIENTO ----------
 caracteres = ""
 
 if usar_letras == "s":
@@ -26,16 +26,16 @@ if usar_numeros == "s":
 if usar_simbolos == "s":
     caracteres += string.punctuation  # Símbolos
 
-# Validación
+# ---------- VALIDACIÓN ----------
 if caracteres == "":
     print("Error: No se puede generar una contraseña sin caracteres.")
 else:
     contraseña = ""
 
-    # -------- BUCLE --------
+    # ---------- BUCLE ----------
     for i in range(longitud):
         contraseña += random.choice(caracteres)
 
-    # -------- SALIDA --------
+    # ---------- SALIDA ----------
     print("\nContraseña generada:")
     print(contraseña)
